b99852faf4dfa31c62924e6f851c8d2c3119fcc5
marc.duerst <marc.duerst@isolutions.ch>
2016-02-15 08:26:23 +0100

    Added documentation

--- SpecFlowApp/Calculator.cs
+++ SpecFlowApp/Calculator.cs
@@ -1,20 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SpecFlow.App
+﻿namespace SpecFlow.App
 {
+    /// <summary>
+    /// Basic calculator for testing SpecFlow.
+    /// </summary>
     public class Calculator
     {
+        /// <summary>
+        /// Sets the first number for the math operation.
+        /// </summary>
+        /// <value>The first number.</value>
         public int FirstNumber { set; private get; }
+
+        /// <summary>
+        /// Sets the second number for the math operation.
+        /// </summary>
+        /// <value>The second number.</value>
         public int SecondNumber { set; private get; }
 
+        /// <summary>
+        /// Adds <see cref="FirstNumber"/> and <see cref="SecondNumber"/> and
+        /// returns the result.
+        /// </summary>
+        /// <seealso cref="FirstNumber"/>
+        /// <seealso cref="SecondNumber"/>
         public int Add()
         {
             return FirstNumber + SecondNumber;
         }
+
+        /// <summary>
+        /// Subtracts <see cref="SecondNumber"/> from <see cref="FirstNumber"/>
+        /// and returns the result.
+        /// </summary>
+        /// <seealso cref="FirstNumber"/>
+        /// <seealso cref="SecondNumber"/>
         public int Subtract()
         {
             return FirstNumber - SecondNumber;
