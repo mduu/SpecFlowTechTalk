9b69503851f372de26c64b2b510073395063e791
marc.duerst <marc.duerst@isolutions.ch>
2016-02-15 10:45:11 +0100

    Minor fixes

--- SpecFlow.Test/CalculatorSteps.cs
+++ SpecFlow.Test/CalculatorSteps.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using NUnit.Framework;
+﻿using NUnit.Framework;
 using SpecFlow.App;
 using TechTalk.SpecFlow;
 
@@ -13,37 +9,31 @@ namespace SpecFlow.Test
     {
         // For additional details on SpecFlow step definitions see http://go.specflow.org/doc-stepdef
 
-        private Calculator _calculator = new Calculator();
+        private readonly Calculator calculator = new Calculator();
         private int result;
 
         [Given("I have entered (.*) into the calculator")]
         public void GivenIHaveEnteredSomethingIntoTheCalculator(int number)
         {
-            //TODO: implement arrange (precondition) logic
-            // For storing and retrieving scenario-specific data see http://go.specflow.org/doc-sharingdata 
-            // To use the multiline text or the table argument of the scenario,
-            // additional string/Table parameters can be defined on the step definition
-            // method. 
-
-            _calculator.FirstNumber = number;
+            calculator.FirstNumber = number;
         }
 
         [Given(@"I have also entered (.*) into the calculator")]
         public void GivenIHaveAlsoEnteredIntoTheCalculator(int number)
         {
-            _calculator.SecondNumber = number;
+            calculator.SecondNumber = number;
         }
 
         [When("I press add")]
         public void WhenIPressAdd()
         {
-            result = _calculator.Add();
+            result = calculator.Add();
         }
 
         [When("I press subtract")]
         public void WhenIPressSubtract()
         {
-            result = _calculator.Subtract();
+            result = calculator.Subtract();
         }
 
         [Then("the result should be (.*) on the screen")]
